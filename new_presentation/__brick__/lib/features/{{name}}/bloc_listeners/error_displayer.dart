6b1e1332d357a987fefef62209a3495bc5aacf58
SemihYlmzz <ekkotaymmm@gmail.com>
2024-08-29 09:14:43 +0300

    # App path fixed.

--- new_presentation/__brick__/lib/features/{{name}}/bloc_listeners/error_displayer.dart
+++ new_presentation/__brick__/lib/features/{{name}}/bloc_listeners/error_displayer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../app/app.dart';
+import '../../../app/app.dart';
 import '../{{name}}.dart';
 
 extension {{name.pascalCase()}}ErrorDisplayer on {{name.pascalCase()}}BlocListeners {
--- new_presentation/__brick__/lib/features/{{name}}/cubit/{{name}}_cubit.dart
+++ new_presentation/__brick__/lib/features/{{name}}/cubit/{{name}}_cubit.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../app/app.dart';
+import '../../../app/app.dart';
 
 part '{{name}}_cubit.freezed.dart';
 part '{{name}}_cubit_state.dart';
