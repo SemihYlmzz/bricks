a6bb788b9b39441079d6b529c9d49ce15428e263
SemihYlmzz <ekkotaymmm@gmail.com>
2024-09-02 16:30:18 +0300

    # Router updated

--- new_presentation/__brick__/lib/features/{{name}}/router/{{name}}_router.dart
+++ new_presentation/__brick__/lib/features/{{name}}/router/{{name}}_router.dart
@@ -2,27 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../{{name}}.dart';
 
-class {{name.pascalCase()}}Router {
+class {{name.pascalCase()}}Router extends GoRoute {
   factory {{name.pascalCase()}}Router() {
     return instance;
   }
-  {{name.pascalCase()}}Router._();
+  {{name.pascalCase()}}Router._()
+      : super(path: _path, name: _name, pageBuilder: _pageBuilder);
+
   static final {{name.pascalCase()}}Router instance = {{name.pascalCase()}}Router._();
 
   static const String _path = '/{{name}}';
   static const String _name = '{{name}}';
 
-  String get path => _path;
-  String get name => _name;
-
-  GoRoute get route => GoRoute(
-        path: _path,
-        name: _name,
-        // parentNavigatorKey: RouterMixin.parentNavigatorKey,
-        pageBuilder: (context, state) => const MaterialPage(
+  static GoRouterPageBuilder get _pageBuilder => pageBuilder: 
+      (context, state) => const MaterialPage(
           child: {{name.pascalCase()}}Screen(),
-        ),
-      );
+        );
+
   void go(BuildContext context) {
     GoRouter.of(context).goNamed(_name);
   }
